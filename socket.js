241c8ed5f965de4f26527e4f795548a9503d9f92
Abhinav Lakhara <abhinavlakhara8@gmail.com>
2025-05-28 19:29:38 +0530

    Cors problem

--- socket.js
+++ socket.js
@@ -13,7 +13,8 @@ app.use(cors({
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: ['https://videosync-five.vercel.app'],
+        origin: '*',
+        //https://videosync-five.vercel.app
         methods: ["GET","POST"],
         credentials: true,
     }
